390ae7dfc58270984e35cc3a0d716f89d4ec3251
Marian Serna <marianhalliday@gmail.com>
2017-10-03 09:14:01 -0400

    Including headers

--- server.rb
+++ server.rb
@@ -15,14 +15,25 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  header = "HTTP/1.1 200 OK"
   filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
 
   if File.exists?(filename)
     body = File.read(filename)
+    success_header = []
+    success_header << "HTTP/1.1 200 OK"
+    success_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
+    success_header << "Content-Length: #{body.length}" # should be the actual size of the response body
+    success_header << "Connection: close"
+    header = success_header.join("\r\n")
   else
     # body = "File Not Found\n" # need to indicate end of the string with \n
     body = "#{filename} not found"
+    not_found_header = []
+    not_found_header << "HTTP/1.1 404 Not Found"
+    not_found_header << "Content-Type: text/plain" # is always text/plain
+    not_found_header << "Content-Length: #{body.length}" # should the actual size of the response body
+    not_found_header << "Connection: close"
+    header = not_found_header.join("\r\n")
   end
 
   response = [
