72abf901cd52823e097bd372bee56df711960836
Marian Serna <marianhalliday@gmail.com>
2017-10-03 08:51:50 -0400

    Changing body of response

--- server.rb
+++ server.rb
@@ -16,10 +16,26 @@ loop do                                             # Server runs forever
   puts lines                                        # Output the full request to stdout
 
   header = "HTTP/1.1 200 OK"
-  time = Time.now.ctime
-  response = "#{header}\r\n\r\n#{time}"       # Output the HTTP header with current time to the client
+  body = "
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Yoyo my people!</title>
+      </head>
+      <body>
+        <h1>Yoyo my people!</h1>
+        <p>Hello pueblo!</p>
+      </body>
+    </html>"
 
-  client.puts(response)
+  response = [
+    header,
+    body
+  ]
+
+  # time = Time.now.ctime
+  # response = "#{header}\r\n\r\n#{time}"       # Output the HTTP header with current time to the client
+  client.puts(response.join("\r\n\r\n"))
 
   client.close                                      # Disconnect from the client
 end
